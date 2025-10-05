89bb949bce8e9fcced3630d11b60ba30b358ea90
Xnopyt <billy@xnopyt.info>
2025-10-05 21:49:58 +0100

    Decksight brightness fix

--- main.py
+++ main.py
@@ -1,57 +1,52 @@
-import os
-
-# The decky plugin module is located at decky-loader/plugin
-# For easy intellisense checkout the decky-loader code repo
-# and add the `decky-loader/plugin/imports` path to `python.analysis.extraPaths` in `.vscode/settings.json`
-import decky
-import asyncio
-
+import decky, os, struct, ctypes, ctypes.util
 class Plugin:
-    # A normal method. It can be called from the TypeScript side using @decky/api.
-    async def add(self, left: int, right: int) -> int:
-        return left + right
-
-    async def long_running(self):
-        await asyncio.sleep(15)
-        # Passing through a bunch of random data, just as an example
-        await decky.emit("timer_event", "Hello from the backend!", True, 2)
-
-    # Asyncio-compatible long-running code, executed in a task when the plugin is loaded
     async def _main(self):
-        self.loop = asyncio.get_event_loop()
-        decky.logger.info("Hello World!")
+        self.libc = ctypes.CDLL(ctypes.util.find_library("c"), use_errno=True)
+        self.libc.inotify_init.restype = ctypes.c_int
+        self.libc.inotify_add_watch.argtypes = [ctypes.c_int, ctypes.c_char_p, ctypes.c_uint32]
+        self.libc.inotify_add_watch.restype = ctypes.c_int
 
-    # Function called first during the unload process, utilize this to handle your plugin being stopped, but not
-    # completely removed
+        self.EVENT_STRUCT_FORMAT = "iIII"
+        self.EVENT_SIZE = struct.calcsize(self.EVENT_STRUCT_FORMAT)
+        self.IN_MODIFY = 0x00000002
+        
+        self.fd = False
+        self.path = '/sys/class/backlight/amdgpu_bl0/brightness'
+        self.max_path = '/sys/class/backlight/amdgpu_bl0/max_brightness'
+        self.max_brightness = False
+        
     async def _unload(self):
-        decky.logger.info("Goodnight World!")
-        pass
+        if self.fd:
+            os.close(self.fd)
+            self.fd = False
+        
+    async def monitor(self):
+        await self.sendBrightness()
+        
+        self.fd = self.libc.inotify_init()
+        if self.fd < 0:
+            errno = ctypes.get_errno()
+            raise OSError(errno, os.strerror(errno))
 
-    # Function called after `_unload` during uninstall, utilize this to clean up processes and other remnants of your
-    # plugin that may remain on the system
-    async def _uninstall(self):
-        decky.logger.info("Goodbye World!")
-        pass
+        wd = self.libc.inotify_add_watch(self.fd, self.path.encode("utf-8"), self.IN_MODIFY)
+        if wd < 0:
+            errno = ctypes.get_errno()
+            raise OSError(errno, os.strerror(errno))
 
-    async def start_timer(self):
-        self.loop.create_task(self.long_running())
-
-    # Migrations that should be performed before entering `_main()`.
-    async def _migration(self):
-        decky.logger.info("Migrating")
-        # Here's a migration example for logs:
-        # - `~/.config/decky-template/template.log` will be migrated to `decky.decky_LOG_DIR/template.log`
-        decky.migrate_logs(os.path.join(decky.DECKY_USER_HOME,
-                                               ".config", "decky-template", "template.log"))
-        # Here's a migration example for settings:
-        # - `~/homebrew/settings/template.json` is migrated to `decky.decky_SETTINGS_DIR/template.json`
-        # - `~/.config/decky-template/` all files and directories under this root are migrated to `decky.decky_SETTINGS_DIR/`
-        decky.migrate_settings(
-            os.path.join(decky.DECKY_HOME, "settings", "template.json"),
-            os.path.join(decky.DECKY_USER_HOME, ".config", "decky-template"))
-        # Here's a migration example for runtime data:
-        # - `~/homebrew/template/` all files and directories under this root are migrated to `decky.decky_RUNTIME_DIR/`
-        # - `~/.local/share/decky-template/` all files and directories under this root are migrated to `decky.decky_RUNTIME_DIR/`
-        decky.migrate_runtime(
-            os.path.join(decky.DECKY_HOME, "template"),
-            os.path.join(decky.DECKY_USER_HOME, ".local", "share", "decky-template"))
+        while True:
+            data = os.read(self.fd, 4096)
+            offset = 0
+            while offset < len(data):
+                wd, mask, _, _ = struct.unpack_from(self.EVENT_STRUCT_FORMAT, data, offset)
+                offset += self.EVENT_SIZE
+                
+                if mask & self.IN_MODIFY:
+                    await self.sendBrightness()
+                        
+    async def sendBrightness(self):
+        if not self.max_brightness:
+            with open(self.max_path, "r") as file:
+                self.max_brightness = int(file.read())
+        
+        with open(self.path, "r") as file:
+            await decky.emit('brightness_change', int(file.read()), self.max_brightness)
---
+++ src/overlay.tsx
@@ -0,0 +1,80 @@
+
+import { findModuleChild } from "@decky/ui";
+import { 
+    FunctionComponent,
+    useState,
+    useEffect
+} from "react";
+import {
+    addEventListener,
+    removeEventListener
+} from '@decky/api';
+
+enum UIComposition {
+  Hidden = 0,
+  Notification = 1,
+  Overlay = 2,
+  Opaque = 3,
+  OverlayKeyboard = 4,
+}
+
+type UseUIComposition = (composition: UIComposition) => {
+  releaseComposition: () => void;
+};
+
+const useUIComposition: UseUIComposition = findModuleChild((m) => {
+  if (typeof m !== "object") return undefined;
+  for (let prop in m) {
+    if (
+      typeof m[prop] === "function" &&
+      m[prop].toString().includes("AddMinimumCompositionStateRequest") &&
+      m[prop].toString().includes("ChangeMinimumCompositionStateRequest") &&
+      m[prop].toString().includes("RemoveMinimumCompositionStateRequest") &&
+      !m[prop].toString().includes("m_mapCompositionStateRequests")
+    ) {
+      return m[prop];
+    }
+  }
+});
+
+const UICompositionProxy: FunctionComponent = () => {
+  useUIComposition(UIComposition.Notification);
+  return null;
+};
+
+const Overlay = ({ initialOpacity = 0, backgroundColor = 'black' }) => {
+const [opacity, setOpacity] = useState(initialOpacity);
+
+  useEffect(() => {
+    const listener = (current: number, max: number) => {
+        const ratio = Math.max(0, Math.min(1, current / max))
+        const adjusted = Math.pow(ratio, 0.5)
+        setOpacity(Math.round((1 - adjusted) * 100) / 100)
+    };
+
+    addEventListener("brightness_change", listener);
+    return () => {
+      removeEventListener("brightness_change", listener);
+    };
+  }, []);
+
+  return (<div
+    id="brightness_bar_container"
+    style={{
+      left: 0,
+      top: 0,
+      width: "100vw",
+      height: "100vh",
+      background: backgroundColor,
+      zIndex: 7001, // volume bar is 7000
+      position: "fixed",
+      opacity,
+      pointerEvents: "none",
+      transition: "opacity 0.15s linear"
+    }}
+  >
+    <UICompositionProxy />
+  </div>)
+};
+
+export default Overlay;
