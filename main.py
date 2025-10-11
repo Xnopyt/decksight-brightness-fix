2830a9983489a37dec6d4555c5d4fd76cdc1c848
Xnopyt <billy@xnopyt.info>
2025-10-11 17:17:05 +0100

    Use CDP to manage tabs

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import decky, os, struct, ctypes, ctypes.util, asyncio, urllib.request, json
+import decky, os, struct, ctypes, ctypes.util, asyncio, json, aiohttp
 class Plugin:
     async def _main(self):
         self.libc = ctypes.CDLL(ctypes.util.find_library("c"), use_errno=True)
-        self.libc.inotify_init.restype = ctypes.c_int
+        self.libc.inotify_init1.argtypes = [ctypes.c_int]
+        self.libc.inotify_init1.restype = ctypes.c_int
         self.libc.inotify_add_watch.argtypes = [ctypes.c_int, ctypes.c_char_p, ctypes.c_uint32]
         self.libc.inotify_add_watch.restype = ctypes.c_int
 
@@ -14,27 +15,51 @@ class Plugin:
         self.path = '/sys/class/backlight/amdgpu_bl0/brightness'
         self.max_path = '/sys/class/backlight/amdgpu_bl0/max_brightness'
         self.max_brightness = False
+        self.brightness = False
         
         self.loop = asyncio.get_event_loop()
-        self.task = None
+        self.tabsTask = None
         
+        self.session = aiohttp.ClientSession()
+        self.ws = None
+        self.cur_id = 0
+                
         self.blacklistedTabs = ["SharedJSContext", "Steam Shared Context presented by Valve™", "Steam", "SP", "Steam Big Picture Mode"]
-        
+        self.sessions = []
+             
     async def _unload(self):
         if self.fd:
+            self.loop.remove_reader(self.fd)
             os.close(self.fd)
             self.fd = False
             
-        if self.task:
-            self.task.cancel()
+        if self.tabsTask:
+            self.tabsTask.cancel()
+            
+        if self.ws:
+            for session in self.sessions:
+                await self.ws.send_json({
+                    "id": self.get_msg_id(),
+                    "method": "runtime.Evaluate",
+                    "sessionId": session,
+                    "params": {
+                        "expression": "document.children[0].style.filter = null"
+                    }
+                })
+            
+        await self.session.close()
 
     async def start_monitor(self):
-        self.task = self.loop.create_task(self.monitor())
+        while True:
+            try:
+                await self.send_brightness()
+                break
+            except Exception:
+                pass
         
-    async def monitor(self):
-        await self.send_brightness()
+        self.tabsTask = self.loop.create_task(self.monitor_tabs())
         
-        self.fd = self.libc.inotify_init()
+        self.fd = self.libc.inotify_init1(os.O_NONBLOCK)
         if self.fd < 0:
             errno = ctypes.get_errno()
             raise OSError(errno, os.strerror(errno))
@@ -43,16 +68,21 @@ class Plugin:
         if wd < 0:
             errno = ctypes.get_errno()
             raise OSError(errno, os.strerror(errno))
-
-        while True:
+        
+        self.loop.add_reader(self.fd, self.brightness_change)
+        
+    def brightness_change(self):
+        try:
             data = os.read(self.fd, 4096)
             offset = 0
             while offset < len(data):
                 wd, mask, _, _ = struct.unpack_from(self.EVENT_STRUCT_FORMAT, data, offset)
                 offset += self.EVENT_SIZE
-                
+
                 if mask & self.IN_MODIFY:
-                    await self.send_brightness()
+                    self.loop.create_task(self.send_brightness())
+        except BlockingIOError:
+            pass
                         
     async def send_brightness(self):
         if not self.max_brightness:
@@ -61,20 +91,91 @@ class Plugin:
         
         with open(self.path, "r") as file:
             ratio = max(0, min(1, int(file.read()) / self.max_brightness))
-            brightness = pow(ratio, 0.7)
+            self.brightness = pow(ratio, 0.7)
             
-            tabs = self.get_tabs()
+            for session in self.sessions:
+                await self.update_tab_brightness(session)
             
-            await decky.emit('brightness_change', brightness, tabs)
+            await decky.emit('brightness_change', self.brightness, [])
             
-    def get_tabs(self):
-        data = []
-        with urllib.request.urlopen('http://localhost:8080/json') as response:
-            data = json.loads(response.read())
+    def get_msg_id(self):
+        self.cur_id += 1
+        if self.cur_id > 2000000:
+            self.cur_id = 1
+        return self.cur_id
+    
+    async def update_tab_brightness(self, session):
+        if self.ws.closed:
+            return
+        await self.ws.send_json({
+            "id": self.get_msg_id(),
+            "method": "Runtime.evaluate",
+            "sessionId": session,
+            "params": {
+                "expression": "document.children[0].style.filter = 'brightness(" + str(self.brightness) + ")'"
+            }
+        })
+    
+    async def monitor_tabs(self):
+        r = await self.session.get("http://localhost:8080/json/version")
+        data = await r.json()
+        
+        self.ws = await self.session.ws_connect(data['webSocketDebuggerUrl'])
+        await self.ws.send_json({
+            "id": self.get_msg_id(),
+            "method": "Target.setDiscoverTargets",
+            "params": {"discover": True, "filter": [{"type": "page"}]},
+        })
+        
+        pendingSessions = []
+        
+        async for msg in self.ws:
+            if msg.type != aiohttp.WSMsgType.TEXT:
+                continue
+            data = json.loads(msg.data)
             
-        tabs = []
-        for tab in data:
-            if tab['title'] not in self.blacklistedTabs and tab['type'] == 'page':
-                tabs.append(tab['title'])
-                
-        return tabs
+            if "method" in data:
+                if data["method"] == "Target.targetCreated":
+                    if data['params']['targetInfo']['title'] not in self.blacklistedTabs:
+                        msgId = self.get_msg_id()
+                        pendingSessions.append(msgId)
+                        await self.ws.send_json({
+                            "id": msgId,
+                            "method": "Target.attachToTarget",
+                            "params": {
+                                "targetId": data['params']['targetInfo']['targetId'],
+                                "flatten": True
+                            }
+                        })
+                elif data['method'] == "Target.detachedFromTarget":
+                    self.sessions.remove(data['params']['sessionId'])
+                elif data['method'] == "Page.frameNavigated":
+                    #In some cases, when a frame navigates we stop getting events for it.
+                    #This forces us to detach from all sessions and rediscover and attach them.
+                    await self.ws.send_json({
+                        "id": self.get_msg_id(),
+                        "method": "Target.setDiscoverTargets",
+                        "params": {"discover": False},
+                    })
+                    for session in self.sessions:
+                        await self.ws.send_json({
+                            "id": self.get_msg_id(),
+                            "method": "Target.detachFromTarget",
+                            "params": {"sessionId": session},
+                        })
+                    await self.ws.send_json({
+                        "id": self.get_msg_id(),
+                        "method": "Target.setDiscoverTargets",
+                        "params": {"discover": True, "filter": [{"type": "page"}]},
+                    })
+                    
+            elif "id" in data:
+                if data["id"] in pendingSessions:
+                    pendingSessions.remove(data['id'])
+                    self.sessions.append(data['result']['sessionId'])
+                    await self.ws.send_json({
+                        "id": self.get_msg_id(),
+                        "method": "Page.enable",
+                        "sessionId": data['result']['sessionId']
+                    })
+                    await self.update_tab_brightness(data['result']['sessionId'])
--- src/index.tsx
+++ src/index.tsx
@@ -2,39 +2,22 @@ import { staticClasses } from "@decky/ui"
 import {
   definePlugin,
   routerHook,
-  callable,
-  executeInTab,
-  addEventListener,
-  removeEventListener,
+  callable
 } from "@decky/api"
 import { FaShip } from "react-icons/fa"
 import Overlay from "./overlay"
 
 const startMonitor = callable<[], void>("start_monitor")
 
-let cleanupTabs: string[] = []
-
-const listener = (brightness: number, tabs: string[]) => {
-    cleanupTabs = tabs
-    for (const tab of tabs) {
-      executeInTab(tab, true, "document.children[0].style.filter = 'brightness(" + brightness + ")'")
-    }
-}
-
 export default definePlugin(() => {
   routerHook.addGlobalComponent("BrightnessOverlay", (props) => <Overlay {...props} />)
-  addEventListener("brightness_change", listener)
   startMonitor()
   return {
     name: "DeckSight Brightness Fix",
     titleView: <div className={staticClasses.Title}>DeckSight Brightness Fix</div>,
     icon: <FaShip />,
     onDismount() {
-      removeEventListener("brightness_change", listener)
       routerHook.removeGlobalComponent('BrightnessOverlay')
-      for (const tab of cleanupTabs) {
-        executeInTab(tab, false, 'document.children[0].style.filter = null')
-      }
     }
   }
 })
